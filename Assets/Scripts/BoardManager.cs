f944dc48331d819b2b6da0a30624bfd87ad14799
zhe13 <lovewutianzhe@vip.qq.com>
2015-11-08 15:12:32 +0800

    =-=二进制文件同步不了

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -4,6 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Random = UnityEngine.Random;
 
+
 public class BoardManager : MonoBehaviour {
 
 	//serializable to make it easy to store and transmit
