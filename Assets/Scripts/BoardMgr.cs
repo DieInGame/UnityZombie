aeb004d6549d27988c329330695e7b990abc52a3
zhe13 <lovewutianzhe@vip.qq.com>
2016-03-18 10:56:02 +0800

    start my new life

---
+++ Assets/Scripts/BoardMgr.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using Random = UnityEngine.Random;
+
+
+
+public class BoardMgr : MonoBehaviour {
+    [Serializable]
+    public class Count
+    {
+        public int minimum;
+        public int maximum;
+        
+        public Count(int min,int max){
+            minimum = min;
+            maximum = max;
+        }
+        
+    }
+    
+    public int columns = 8;
+    public int rows    = 8;
+    public Count food  = new Count(1,5);
+    public Count wall  = new Count(5,9);
+    public GameObject[] floorTiles;
+    public GameObject[] brickTiles;
+    public GameObject[] enemyTiles;
+    public GameObject[] outWallTiles;
+    
+    private Transform
+    
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
